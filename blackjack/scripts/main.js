a1cc8b2caa230d5cdcb8fdf89d2c04ad5e489238
Robert Hutchins <rjhutchins22@gmail.com>
2017-06-13 11:14:31 -0400

    Finish algorithm for handValue function

--- blackjack/scripts/main.js
+++ blackjack/scripts/main.js
@@ -5,15 +5,49 @@
    to watch the assertions pass as you write your code.
 
    Also remember, that the parameter `hand` will be an array, so
-   you'll need to parse through that first before you can start to 
+   you'll need to parse through that first before you can start to
    write your logic.
 */
 
 
-function handValue (hand) {
+function handValue (hand)
+{
+  let value = 0;
+  let blackjack = 21;
 
+  for (let i = 0; i < hand.length; i++)
+  {
+    if(hand[i] === "2" || hand[i] === "3" || hand[i] === "4" || hand[i] === "5" || hand[i] === "6" || hand[i] === "7" || hand[i] === "8" || hand[i] === "9" || hand[i] === "10")
+    {
+         let cardVal = hand[i]
+         value = value + parseInt(cardVal);
+    }
+    else if(hand[i] === "K" || hand[i] === "Q" ||hand[i] === "J")
+    {
+      let placeHolderValue = 10;
+      value = value + placeHolderValue;
+    }
+    else if (hand[i] === "A")
+    {
+      let option1 = 1;
+      let option2 = 11;
 
-  return;
+      if((value + option2) > blackjack)
+      {
+        value = value + option1;
+      }
+      else
+      {
+        value = value + option2;
+      }
+    }
+  }
+  if(value > blackjack)
+  {
+    value = value - 10;
+  }
+  console.log(value);
+  return value;
 }
 
 
